1ab9ad850491cc1ba83058d58619cd95ce4409fb
SirRichardM <richardmoeb@gmail.com>
2020-03-09 13:52:04 -0400

    hello world

---
+++ knowledgeinc/src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header() {
+  
+
+
+  return (
+    <div>
+      <h1>Knowledge</h1>
+      <nav>
+        <a>Home</a>
+        <a>Shop</a>
+        <a>Contact</a>
+      </nav>
+    </div>
+  )
+}
